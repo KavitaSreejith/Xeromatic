a555e779fb21e5c1ab322b33e70b01100c1c4471
kavi2685 <kavi2685@gmail.com>
2016-04-09 13:00:35 +1200

    added a tweet entry amd made a get tweet from API to display recent tweets

--- Project/Xeromatic/Services/TwitterApiService.cs
+++ Project/Xeromatic/Services/TwitterApiService.cs
@@ -2,10 +2,13 @@
 using Tweetinvi;
 using Tweetinvi.Core.Credentials;
 using Tweetinvi.Core.Interfaces;
+using Tweet = Xeromatic.Models.Tweet;
+using Xeromatic.Services;
+using System.Linq;
 
 namespace Xeromatic.Services
 {
-    public class TwitterApiService
+    public class TwitterApiService : ITwitterService
     {
         // Get keys from: https://apps.twitter.com
         // Wiki for tweetinvi: https://github.com/linvi/tweetinvi/wiki
@@ -16,22 +19,31 @@ namespace Xeromatic.Services
         {
             _creds = new TwitterCredentials
             {
-                ConsumerKey = "Add your ConsumerKey here",
-                ConsumerSecret = "Add your ConsumerSecret here",
-                AccessToken = "Add your AccessToken here",
-                AccessTokenSecret = "Add your AccessTokenSecret here"
+                ConsumerKey = "2It9jxXfBg00eazrlSF0rv0pI",
+                ConsumerSecret = "g1aKfmizybFCVY9l8EtuIeYJ7oHIvUWG8pJ5fjRxzd6YR93ydh",
+                AccessToken = "113949626-g7dqvgK0j1xvns8vE9BJRDFtkzwwNoTJemDuFGj7",
+                AccessTokenSecret = "nVHoX382AF8OKnxFpZaTNOCw4Obc0G4mXVZzFXZ4k9IFI"
             };
+
+
         }
 
-        public IEnumerable<ITweet> GetTweets()
+        public IEnumerable<Tweet> GetTweets()
         {
-            var tweets = Auth.ExecuteOperationWithCredentials(_creds, () =>
+            var tweets = Auth.ExecuteOperationWithCredentials(_creds, () => Timeline.GetUserTimeline("xero", 10)).ToList();
+            if (tweets.Any())
             {
-                return Timeline.GetHomeTimeline();
-            });
 
-            return tweets;
+                return tweets.Select(t => new Tweet
+                {
+                    Id = t.Id,
+                    Text = t.Text
+                });
+            }
+            return new List<Tweet>();
+
         }
-
     }
 }
+
+    
